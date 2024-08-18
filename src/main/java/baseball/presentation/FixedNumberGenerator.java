e6bc1555ddeddd73943050e6b41529b4fc10e28d
mincheolkk <kmc04080408@gmail.com>
2024-08-18 17:08:46 +0900

    test : 숫자 생성이 정상적으로 이루어지는지 비교

---
+++ src/main/java/baseball/presentation/FixedNumberGenerator.java
@@ -0,0 +1,9 @@
+package baseball.presentation;
+
+public class FixedNumberGenerator implements NumberGenerator {
+
+    @Override
+    public int generate() {
+        return 1;
+    }
+}
--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -2,18 +2,20 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
+import baseball.presentation.FixedNumberGenerator;
+import baseball.presentation.NumberGenerator;
+import baseball.presentation.RandomNumberGenerator;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 class ComputerTest {
 
-    private Computer computer;
+    private NumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+    private NumberGenerator fixedNumberGenerator = new FixedNumberGenerator();
 
-    @BeforeEach
-    void setUp() {
-        computer = new Computer();
-    }
+    private Computer computer = new Computer(randomNumberGenerator);
 
     @Test
     void 길이를_검증한다() {
@@ -31,5 +33,46 @@ class ComputerTest {
 
         // then
         assertThat(result).doesNotHaveDuplicates();
+    }
+
+    @Test
+    void 랜덤_숫자생성기를_주입받으면_숫자생성이_정상완료된다() throws InterruptedException {
+        // given
+        CountDownLatch latch = new CountDownLatch(1);
+        Thread testThread = new Thread(() -> {
+            try {
+                computer.generateRandomDigits();
+            } finally {
+                latch.countDown();
+            }
+        });
+
+        // when
+        testThread.start();
+
+        // then
+        boolean result = latch.await(1, TimeUnit.SECONDS);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 고정된_숫자생성기를_주입받으면_무한루프에_빠진다() throws InterruptedException {
+        // given
+        Computer fakeComputer = new Computer(fixedNumberGenerator);
+        CountDownLatch latch = new CountDownLatch(1);
+        Thread testThread = new Thread(() -> {
+            try {
+                fakeComputer.generateRandomDigits();
+            } finally {
+                latch.countDown();
+            }
+        });
+
+        // when
+        testThread.start();
+
+        // then
+        boolean result = latch.await(1, TimeUnit.SECONDS);
+        assertThat(result).isFalse();
     }
 }
